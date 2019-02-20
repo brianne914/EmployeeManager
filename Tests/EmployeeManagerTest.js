a0ea84f4b3f761bc1c7144e1b2b8b60c3d9c5429
atrain smith <andrew.smithno2@gmail.com>
2019-02-20 14:19:22 -0700

    Initial Commit

---
+++ Tests/EmployeeManagerTest.js
@@ -0,0 +1,42 @@
+var employeeManagerPage = {}
+
+var validChanges1 = require('../TestAssets/ValidChanges1')
+var validChanges2 = require('../TestAssets/ValidChanges2')
+var invalidChanges = require('../TestAssets/InvalidChanges')
+
+module.exports = {
+    beforeEach: browser => {
+        employeeManagerPage = browser.page.EmployeeManagerPage()
+        employeeManagerPage.navigate()
+        console.log()
+    },
+
+    after: browser => {
+        browser.end()
+    },
+
+    'Employee 1 Changes': browser => {
+        validChanges1(employeeManagerPage, 'Andrew', '1231231234', 'Mentor')
+
+    },
+
+    'Invalid Test': browser => {
+        invalidChanges(employeeManagerPage, 'Andrew Smith is the best mentor i swear to Batman', '12345678901234567890')
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+// .getText('@fastestLeviathan', function (result) {
+//     console.log(result.value)
+// })
